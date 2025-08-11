cfefcf6ba298a79986aa94d873b992c16ea4b6b2
Mathias Soria <mathiassoriapiro@gmail.com>
2025-08-11 00:13:23 -0300

    Agregado ServicioMensajeria desde prueba de factibilidad. Por ahora con metodo statico enviar  para test.

---
+++ src/persistencia/ServicioMensajeria.java
@@ -0,0 +1,76 @@
+package persistencia;
+import java.util.Properties;
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import java.io.File;
+import java.io.IOException;
+
+
+public class ServicioMensajeria {
+	String micorreo = "tallerINET2025@gmail.com";
+	String clave = "wjvj lssx adib dinn";
+	
+	String destino;
+	String asunto;
+	String textoMensaje;	
+	String cuerpoHtml;
+	String rutaAdjunto;
+	
+	public static void enviar(String textoMensaje, String destino) {		
+		// Cuerpo HTML del correo
+       /*	String cuerpoHtml = "<h2 style='color: navy;'>Hola desde Java</h2> "
+	 		+ "<p>Este es un correo <strong>HTML</strong> con archivo adjunto.<br> "
+	 		+ "¡Saludos!</p> "
+	 		+ "";*/
+		
+		Properties propiedades = new Properties();
+        propiedades.put("mail.smtp.auth", "true");
+        propiedades.put("mail.smtp.host", "smtp.gmail.com");
+        propiedades.put("mail.smtp.port", "465");
+        propiedades.put("mail.smtp.ssl.enable", "true");
+        propiedades.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        
+        Session sesion = Session.getInstance(propiedades, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(micorreo, clave);
+            }
+        });
+        
+        try {
+        	 Message mail = new MimeMessage(sesion);
+        	 mail.setFrom(new InternetAddress(micorreo));
+        	 mail.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destino));
+        	 mail.setSubject(asunto);
+        	 
+        	// Parte de texto
+             //MimeBodyPart cuerpoTexto = new MimeBodyPart();
+             //cuerpoTexto.setText(mensaje);
+             
+             MimeBodyPart parteHtml = new MimeBodyPart();
+             parteHtml.setContent(cuerpoHtml, "text/html; charset=utf-8");
+
+             // Parte de archivo adjunto
+             MimeBodyPart adjunto = new MimeBodyPart();
+             adjunto.attachFile(new File(rutaAdjunto));
+
+             // Unimos ambas partes en un contenedor Multipart
+             Multipart multipart = new MimeMultipart();
+             multipart.addBodyPart(parteHtml);
+             //multipart.addBodyPart(cuerpoTexto);
+             multipart.addBodyPart(adjunto);
+
+             // Enviamos el contenido completo
+             mail.setContent(multipart);
+             Transport.send(mail);
+             System.out.println("Correo enviado correctamente.");
+        }
+        catch (MessagingException e){
+        	e.printStackTrace();
+        }
+        catch (IOException e) {
+        	e.printStackTrace();
+        }
+	
+
+	}
+}
