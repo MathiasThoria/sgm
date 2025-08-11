bd47895e870201ca408477e692e52fd930f57051
Mathias Soria <mathiassoriapiro@gmail.com>
2025-08-10 23:41:18 -0300

    agregarMensaje a Historias

--- src/modelo/Historias.java
+++ src/modelo/Historias.java
@@ -5,5 +5,13 @@ import java.util.LinkedList;
 public class Historias {
 	LinkedList<MensajeEnviado> mensajesEnviados;
 	
+	public Historias() {
+		mensajesEnviados=new LinkedList<MensajeEnviado>();
+	}
+	/*Agrega mensaje a coleccion. Modifica id a autonumerico*/
+	public void agregarMensaje(MensajeEnviado mensaje) {
+		mensaje.setId(this.mensajesEnviados.size()+1);
+		this.mensajesEnviados.add(mensaje);
+	}
 	
 }
