15530c0827ea1484d923acbd9d46af69c861293f
Mathias Soria <mathiassoriapiro@gmail.com>
2025-09-09 21:01:15 -0300

    fix testing

--- src/test/testManejadorDatos.java
+++ src/test/testManejadorDatos.java
@@ -1,11 +1,12 @@
 package test;
 
-import logica.ManejadorDatos;
+import logica.*;
+import modelo.*;
 
 public class testManejadorDatos {
 
 	public static void main(String[] args) throws Exception {
-		ManejadorDatos md = new ManejadorDatos();
+		ManejadorDatos md = new ManejadorDatos(new Usuarios());
 		md.parserDatosToUsuarios();
 		System.out.println(md.getColeccionUsuario().toString());
 
