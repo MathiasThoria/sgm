d252058a476baf607ee04db85ff4c36d1c38f0a9
Mathias Soria <mathiassoriapiro@gmail.com>
2025-09-24 19:16:40 -0300

    fix comments

--- src/logica/Controlador.java
+++ src/logica/Controlador.java
@@ -3,10 +3,13 @@ import modelo.*;
 import persistencia.HistoriasBD;
 /**
  * Responsabilidades: 
- * - orquestar entre vista y modelo
+ * - logica entre vista y modelo
  * - mantener instancias de modelo
  * - mantener instancias de manejadores
+ * - logica entre modelo y persistencia. 
  * 
+ * Manejadores: 
+ *  - centralizan operaciones con persistencia (BD y XLS)
  * Los manejadores tienen inyeccion de dependencia (le "paso" usuarios y historias para que lo tnegan como atributo)
  * Al instanciar el controlador se actualiza el modelo desde BD y xls
  **/
@@ -31,7 +34,7 @@ public class Controlador {
         actualizarModeloDesdeBD(); 
     }
    
-   
+    // ------ MODELO ---------- // 
     public String obtenerUsuarios() {    	
     	return usuarios.obtenerDatosUsuarios();
     }
@@ -42,78 +45,19 @@ public class Controlador {
     public boolean existeUsuario(int id) {    	
     	return usuarios.existeIdUsuario(id);    
     }
-    
-    
-    public int procesarMensajesDeUsuarios() {    	
-    	int res= manejadorMensajes.procesarEnvioMensajes(manejadorDatos.getColeccionUsuario());
-    	if ( res > 0 )
-    		//manejadorBD.persistirMensajesNuevos(manejadorMensajes.getMensajesSinPersistir()); 
-    		manejadorBD.persistirMensajesNuevos();
-    	return res;
-    }
-    
     public String obtenerHistoricoMensajes() {    	
     	return historias.toString();
-    }
-    
+    }    
     public String obtenerHistoricoMensajesPorIdUsuario(int id) {    	
     	return historias.obtenerMensajesPorIdUsuario(id);
     }
-    
-    public void borrarTodasHistoriasBD() {
-    	manejadorBD.borrarTodasHistoriasBD();
-    	historias.borrarTodo();
-    	
-    }
-    public void actualizarModeloDesdeBD() {
-    	manejadorBD.cargarModelo();
-    }
-    public void actualizarModeloDesdeDatosXls() {
-    	manejadorDatos.cargarModelo();
-    }
-
     public String obtenerUsuariosDelSistema() {
     	return usuariosSistema.obtenerUsuarios();
-    }
-    public void altaUsuarioSistema(int id, String perfil, String contraseña) {    	
-    	usuariosSistema.agregarUsuario(id,perfil,contraseña);
-    	manejadorBD.agregarUsuarioSistema(id,perfil,contraseña);
-    }
-    public boolean eliminarUsuarioSistema(int id) {
-    	boolean res=false;
-    	if (usuariosSistema.existeId(id)) {
-    		usuariosSistema.eliminarUsuario(id);
-    		manejadorBD.bajaUsuarioSistema(id);
-    		res=true;
-    	}
-    	return res;
     }
     public String obtenerUsuarioDelSistema(int id) {
     	return usuariosSistema.obtenerUsuarioPorId(id);
     }
-    public boolean modificarUsuarioDelSistema(int id, String perfil, String contraseña) {
-    	boolean res=false;
-    	
-    	if (usuariosSistema.modificarUsuario(id,perfil,contraseña) && 
-    			manejadorBD.modificarUsuarioSistema(id,perfil,contraseña))
-    		res=true;
-    	return res;
-    }
-    public boolean eliminarMensajeEnviado(int id) {
-    	boolean res=false;
-    	if (historias.eliminarMensajeEnviado(id) && manejadorBD.eliminarMensajeEnviado(id))
-    		res=true;
-    	return res;
-    }
-    public boolean verificarContraseñaUsuarioSistema(int id, String contraseña) {
-		return manejadorBD.verificarContraseñaUsuarioSistema(id,contraseña);		
-	}
-    public String obtenerPerfilUsuarioSistema(int id) {
-    	return manejadorBD.obtenerPerfilUsuarioSistema(id);
-    }
-    
-    public String obtenerConstancia(int id) {
-    	
+    public String obtenerConstancia(int id) {    	
     	Fecha fecha = new Fecha();
     	fecha.establecerComoHoy();
     	
@@ -125,6 +69,68 @@ public class Controlador {
     	
     	return constancia;
     }
+    // ---------- MENSAJES ------------//
+    public int procesarMensajesDeUsuarios() {    	
+    	int res= manejadorMensajes.procesarEnvioMensajes(manejadorDatos.getColeccionUsuario());
+    	if ( res > 0 )
+    		//manejadorBD.persistirMensajesNuevos(manejadorMensajes.getMensajesSinPersistir()); 
+    		manejadorBD.persistirMensajesNuevos();
+    	return res;
+    }
+    
+    
+    // ------------ BD ----------------//
+    public void borrarTodasHistoriasBD() {
+    	manejadorBD.borrarTodasHistoriasBD();
+    	historias.borrarTodo();
+    	
+    }
+    public void actualizarModeloDesdeBD() {
+    	manejadorBD.cargarModelo();
+    }
+    public boolean verificarContraseñaUsuarioSistema(int id, String contraseña) {
+		return manejadorBD.verificarContraseñaUsuarioSistema(id,contraseña);		
+	}    
+    public String obtenerPerfilUsuarioSistema(int id) {
+    	return manejadorBD.obtenerPerfilUsuarioSistema(id);
+    }
+    
+    
+    // ------------- DATOS XLS ------------//
+    
+    public void actualizarModeloDesdeDatosXls() {
+    	manejadorDatos.cargarModelo();
+    }    
+    
+    
+    // -------------- MODELO Y BD ----------------//
+    public void altaUsuarioSistema(int id, String perfil, String contraseña) {    	
+    	usuariosSistema.agregarUsuario(id,perfil,contraseña);
+    	manejadorBD.agregarUsuarioSistema(id,perfil,contraseña);
+    }
+    
+    public boolean eliminarUsuarioSistema(int id) {
+    	boolean res=false;
+    	if (usuariosSistema.existeId(id)) {
+    		usuariosSistema.eliminarUsuario(id);
+    		manejadorBD.bajaUsuarioSistema(id);
+    		res=true;
+    	}
+    	return res;
+    }    
+    public boolean modificarUsuarioDelSistema(int id, String perfil, String contraseña) {
+    	boolean res=false;    	
+    	if (usuariosSistema.modificarUsuario(id,perfil,contraseña) && 
+    			manejadorBD.modificarUsuarioSistema(id,perfil,contraseña))
+    		res=true;
+    	return res;
+    }    
+    public boolean eliminarMensajeEnviado(int id) {
+    	boolean res=false;
+    	if (historias.eliminarMensajeEnviado(id) && manejadorBD.eliminarMensajeEnviado(id))
+    		res=true;
+    	return res;
+    }    
     
 }
 
